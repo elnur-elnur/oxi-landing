736224a460ffe77e2b6f64a6a138c6c585a28c4e
Elnur <tahirzadee@gmail.com>
2022-04-29 15:23:46 +0400

    created oxi landing

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+var scene2 = document.getElementById('scene2');
+var parallax = new Parallax(scene2);
+
+// for footer
+var scene3 = document.getElementById('scene3');
+var parallax = new Parallax(scene3);
+
+var scene4 = document.getElementById('scene4');
+var parallax = new Parallax(scene4);
+
+// for info section
+
+var scene5 = document.getElementById('scene5');
+var parallax = new Parallax(scene5);
+
+var scene6 = document.getElementById('scene6');
+var parallax = new Parallax(scene6);
+
+var scene7 = document.getElementById('scene7');
+var parallax = new Parallax(scene7);
